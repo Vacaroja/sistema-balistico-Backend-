3e7c8473467c0ec8f24ac724c3159d44c0383f38
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2026-05-05 09:16:44 -0400

    git init

---
+++ src/main/java/com/ccc/sistema_balistico/SistemaBalisticoApplication.java
@@ -0,0 +1,13 @@
+package com.ccc.sistema_balistico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBalisticoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaBalisticoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ccc/sistema_balistico/SistemaBalisticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccc.sistema_balistico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBalisticoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
