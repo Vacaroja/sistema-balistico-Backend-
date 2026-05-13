c5c0e383fd2895b4f5235f2c194cbc2b42d24bbd
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2026-05-12 22:11:55 -0400

    feat: implement BulletMapper for DTO conversion

---
+++ src/main/java/com/ccc/sistema_balistico/dto/mapper/BulletMapper.java
@@ -0,0 +1,35 @@
+package com.ccc.sistema_balistico.dto.mapper;
+
+import com.ccc.sistema_balistico.dto.BulletDTO;
+import com.ccc.sistema_balistico.entities.BulletEntity;
+import com.ccc.sistema_balistico.entities.CaliberEntity;
+
+public class BulletMapper {
+    public static BulletDTO toDTO(BulletEntity bulletEntity){
+        return BulletDTO.builder().
+                idBullet(bulletEntity.getIdBullet()).
+                caseFile(bulletEntity.getCaseFile()).
+                landsAndGrooves(bulletEntity.getLandsAndGrooves()).
+                percussionType(bulletEntity.getPercussionType()).
+                twistDirection(bulletEntity.getTwistDirection()).
+                caliber(bulletEntity.getCaliberEntity().getIdCaliber()).
+                manufacturer(bulletEntity.getManufacturer()).
+                createdAt(bulletEntity.getCreatedAt()).
+                build();
+
+    }
+    public static BulletEntity toEntity(BulletDTO bulletEntity, CaliberEntity caliberEntity){
+        return BulletEntity.builder().
+                idBullet(bulletEntity.getIdBullet()).
+                caseFile(bulletEntity.getCaseFile()).
+                landsAndGrooves(bulletEntity.getLandsAndGrooves()).
+                percussionType(bulletEntity.getPercussionType()).
+                twistDirection(bulletEntity.getTwistDirection()).
+                caliberEntity(caliberEntity).
+                manufacturer(bulletEntity.getManufacturer()).
+                createdAt(bulletEntity.getCreatedAt()).
+                build();
+
+    }
+
+}
