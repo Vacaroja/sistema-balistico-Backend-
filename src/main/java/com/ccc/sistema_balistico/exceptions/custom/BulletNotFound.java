31a69447f712a9a2bcfa1692307b86b469f814f6
Andrés Alfonzo <vr.alfonzo1@gmail.com>
2026-05-12 22:11:44 -0400

    feat: add custom exceptions for Bullet and Caliber not found

---
+++ src/main/java/com/ccc/sistema_balistico/exceptions/custom/BulletNotFound.java
@@ -0,0 +1,7 @@
+package com.ccc.sistema_balistico.exceptions.custom;
+
+public class BulletNotFound extends RuntimeException {
+    public BulletNotFound(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ccc/sistema_balistico/exceptions/custom/caliber/CaliberNotFound.java
@@ -0,0 +1,7 @@
+package com.ccc.sistema_balistico.exceptions.custom.caliber;
+
+public class CaliberNotFound extends RuntimeException {
+    public CaliberNotFound(String message) {
+        super(message);
+    }
+}
